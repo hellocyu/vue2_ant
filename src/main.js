90614b39a3abb9d47dcb159b4c4d3fd8711bb40a
hellocyu <hellocyu.163.com>
2023-04-27 19:44:13 +0800

    #feat:20230427引入echarts

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 import router from "./router";
+import * as echarts from "echarts";
+Vue.prototype.$echarts = echarts;
 Vue.config.productionTip = false;
 Vue.use(Antd);
 
--- src/views/Member/memberInfo.vue
+++ src/views/Member/memberInfo.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>会员信息</div>
+  <div>
+    <div ref="chartColumn" class="chartStyle"></div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "memberInfo",
+  data() {
+    return {
+      option: {
+        xAxis: {
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {},
+        series: [
+          {
+            type: "bar",
+            data: [23, 24, 18, 25, 27, 28, 25],
+          },
+        ],
+      },
+    };
+  },
+  mounted() {
+    this.initChart();
+  },
+  methods: {
+    initChart() {
+      this.chartColumn = this.$echarts.init(this.$refs.chartColumn);
+      this.chartColumn.setOption(this.option);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.chartStyle {
+  width: 100%;
+  height: 400px;
+}
+</style>
